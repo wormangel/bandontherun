309856d86e05c7725e6a6c4ed0045be49136a2f7
Otacilio Lacerda <otacilio@otacilio-desktop.(none)>
2011-08-28 23:06:08 -0300

    Add User and Band Form

--- project/urls.py
+++ project/urls.py
@@ -9,20 +9,16 @@ urlpatterns = patterns('',
     url(r'^about$', 'project.views.about', name='about'),
 
     # band
-    url(r'^band/new$', 'project.views.new_band', name='new-band'),
     url(r'^band/create$', 'project.views.create_band', name='create-band'),
     url(r'^band/(?P<shortcut_name>\w+)$', 'project.views.show_band', name='show-band'),
     url(r'^band/(?P<shortcut_name>\w+)/edit$', 'project.views.edit_band', name='edit-band'),
-    url(r'^band/(?P<shortcut_name>\w+)/update$', 'project.views.update_band', name='update-band'),
     url(r'^band/(?P<shortcut_name>\w+)/member/add$', 'project.views.add_band_member', name='add-band-member'),
 
     # accounts
     url(r'^user/login$', 'project.views.login', name='user-login'),
     url(r'^user/logout$', 'project.views.logout', name='user-logout'),
-    url(r'^user/new$', 'project.views.new_user', name='new-user'),
     url(r'^user/create$', 'project.views.create_user', name='create-user'),
     url(r'^user/edit$', 'project.views.edit_user', name='edit-user'),
-    url(r'^user/update$', 'project.views.update_user', name='update-user'),
     url(r'^user/dashboard$', 'project.views.dashboard', name='user-dashboard'),
     url(r'^user/invite$', 'project.views.invite_user', name='invite-user'),
     url(r'^user/profile/(?P<username>\w+)$', 'project.views.show_user', name='show-user'),
--- project/views.py
+++ project/views.py
@@ -2,12 +2,13 @@
 # TODO: create decorators for @get @post @put @delete
 # TODO: change some posts request to put (investigate how to do that with django)
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login as login_auth, logout as logout_auth
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 
 from models import Band
+from forms import *
 
 import users_manager, bands_manager
 
@@ -29,13 +30,6 @@ def dashboard(request):
 def show_user(request, username):
      return render_to_response('user/show.html', context_instance=RequestContext(request))
 
-def new_user(request):
-    if request.method == 'GET':
-        return render_to_response('user/new.html', context_instance=RequestContext(request))
-    else:
-        # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
-        return HttpResponse(status=405)
-
 def invite_user(request):
     if request.method == 'POST':
         email = request.POST['email']
@@ -43,50 +37,40 @@ def invite_user(request):
 
 def create_user(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
-        phone = request.POST['phone']
-        # TODO: validate input / use form
-        users_manager.create_user(first_name, last_name, username, password, email, phone)
-        user = authenticate(username=username, password=password)
-        if user is not None:
+        form = UserForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+        
+            users_manager.create_user(first_name, last_name, username, password, email, phone)
+            
+            # authenticate and login
+            user = authenticate(username=username, password=password)
             login_auth(request, user)
-            return redirect('/')
-        else:
-            # TODO: append validation errors
-            return redirect('/user/new')
+            return HttpResponseRedirect('/user/dashboard')
     else:
-        # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
-        return HttpResponse(status=405)
+        form = UserForm()
+    return render_to_response('user/create.html', { 'form' : form }, context_instance=RequestContext(request))
 
 def edit_user(request):
-    if request.method == 'GET':
-        return render_to_response('user/edit.html', context_instance=RequestContext(request))
-    else:
-        # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
-        return HttpResponse(status=405)
-
-def update_user(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
-        phone = request.POST['phone']
-        # TODO: validate input / use form
-        user = users_manager.update_user(request.user, first_name, last_name, username, password, email, phone)
-        if user is not None:
-            return redirect('/user/dashboard')
-        else:
-            # TODO: append validation errors
-            return redirect('/user/edit')
+        form = UserForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            user = users_manager.update_user(request.user, first_name, last_name, username, password, email, phone)
+            return HttpResponseRedirect('/user/dashboard')
     else:
-        # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
-        return HttpResponse(status=405)
+        form = UserForm()
+    return render_to_response('user/edit.html', { 'form' : form }, context_instance=RequestContext(request))
 
 def login(request):
     if request.method == 'GET':
@@ -98,10 +82,12 @@ def login(request):
             next = request.GET['next']
         except:
             next = '/user/dashboard'
+        print username
+        print password
         user = authenticate(username=username, password=password)
         if user is not None:
             login_auth(request, user)
-            return redirect(next)
+            return HttpResponseRedirect(next)
         else:
             # TODO: append errors
             return redirect('/user/login?next=%s' % next)
@@ -123,50 +109,33 @@ def show_band(request, shortcut_name):
         # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
         return HttpResponse(status_code=405)
 
-def new_band(request):
-    if request.method == 'GET':
-        return render_to_response('band/new.html', context_instance=RequestContext(request))
-    else:
-        # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
-        return HttpResponse(status_code=405)
-
 def create_band(request):
     if request.method == 'POST':
-        band_name = request.POST['band_name']
-        shortcut_name = request.POST['shortcut_name']
-        bio = request.POST['bio']
-        url = request.POST['url']
-        # TODO: validate input / use form
-        bands_manager.create_band(band_name, shortcut_name, bio, url, request.user)
-        return redirect('/band/%s' % shortcut_name)
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band_name = form.cleaned_data['band_name']
+            shortcut_name = form.cleaned_data['shortcut_name']
+            bio = form.cleaned_data['bio']
+            url = form.cleaned_data['url']
+            bands_manager.create_band(band_name, shortcut_name, bio, url, request.user)
+            return HttpResponseRedirect('/band/%s' % shortcut_name)
     else:
-        # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
-        return HttpResponse(status_code=405)
+        form = BandForm()
+    return render_to_response('band/create.html', { 'form' : form }, context_instance=RequestContext(request))
 
 def edit_band(request, shortcut_name):
-    if request.method == 'GET':
-        return render_to_response('band/edit.html', {'band': bands_manager.get_band(shortcut_name)}, context_instance=RequestContext(request))
-    else:
-        # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
-        return HttpResponse(status_code=405)
-
-def update_band(request, shortcut_name):
     if request.method == 'POST':
-        band_name = request.POST['band_name']
-        new_shortcut_name = request.POST['new_shortcut_name']
-        bio = request.POST['bio']
-        url = request.POST['url']
-        # TODO: validate input / use form
-        # validate if user updating the info is a member
-        if band is not None:
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band_name = form.cleaned_data['band_name']
+            shortcut_name = form.cleaned_data['shortcut_name']
+            bio = form.cleaned_data['bio']
+            url = form.cleaned_data['url']
             bands_manager.update_band(band_name, shortcut_name, new_shortcut_name, bio, url)
-        else:
-            # echo input vars on output
-            return redirect('/band/%s/edit' % shortcut_name)
-        return redirect('/band/%s' % band_name)
+            return HttpResponseRedirect('/band/%s' % band_name)
     else:
-        # The response MUST include an Allow header containing a list of valid methods for the requested resource. 
-        return HttpResponse(status_code=405)
+        form = BandForm()
+    return render_to_response('band/edit.html', {'band': bands_manager.get_band(shortcut_name), 'form' : form}, context_instance=RequestContext(request))
         
 def add_band_member(request, shortcut_name):
     if request.method == 'POST':
