ac58bb58aed88108b0dd14bb4181a08e9aacf51b
Lucas Medeiros de Azevedo <wormangel@gmail.com>
2011-09-16 00:43:07 -0300

    Password recovery and logging mail settings

--- settings_deploy.py
+++ settings_deploy.py
@@ -1,5 +1,13 @@
 from settings import *
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bandontherundev@gmail.com'
+EMAIL_HOST_PASSWORD = '' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
