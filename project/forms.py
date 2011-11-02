0d51c2f493e0230c1b174b39b84e42bbe3bee7bb
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-11-02 09:25:29 -0300

    displaying time and money fields correctly now

--- project/forms.py
+++ project/forms.py
@@ -55,21 +55,21 @@ class UnavailabilityEntryForm(forms.Form):
     all_day = forms.BooleanField(label="All day?", required=False)
     date_start = forms.DateField()
     date_end = forms.DateField()
-    time_start = forms.TimeField(widget=forms.TimeInput(), input_formats=["%H:%M"])
-    time_end = forms.TimeField(widget=forms.TimeInput(), input_formats=["%H:%M", "%H:%M:%S"])
+    time_start = forms.TimeField(widget=forms.TimeInput(format='%H:%M'), input_formats=["%H:%M"])
+    time_end = forms.TimeField(widget=forms.TimeInput(format='%H:%M'), input_formats=["%H:%M"])
     
 class RehearsalEntryForm(forms.Form):
     date_start = forms.DateField(label="Date")
-    time_start = forms.TimeField(widget=forms.TimeInput(), input_formats=["%H:%M"])
-    time_end = forms.TimeField(widget=forms.TimeInput(), input_formats=["%H:%M"])
+    time_start = forms.TimeField(widget=forms.TimeInput(format='%H:%M'), input_formats=["%H:%M"])
+    time_end = forms.TimeField(widget=forms.TimeInput(format='%H:%M'), input_formats=["%H:%M"])
     place = forms.CharField(max_length=30)
     costs = forms.DecimalField(max_digits=10, decimal_places=2)
 
 class GigEntryForm(forms.Form):
     date_start = forms.DateField(label="Date start")
     date_end = forms.DateField(label="Date end")
-    time_start = forms.TimeField(widget=forms.TimeInput(), input_formats=["%H:%M"])
-    time_end = forms.TimeField(widget=forms.TimeInput(), input_formats=["%H:%M"])
+    time_start = forms.TimeField(widget=forms.TimeInput(format='%H:%M'), input_formats=["%H:%M"])
+    time_end = forms.TimeField(widget=forms.TimeInput(format='%H:%M'), input_formats=["%H:%M"])
     place = forms.CharField(label="Place/Venue", max_length=30)
     costs = forms.DecimalField(label="Band Total Costs", min_value = 0, max_digits=10, decimal_places=2, required=False)
     ticket = forms.DecimalField(label="Ticket Cost", min_value = 0, max_digits=10, decimal_places=2, required=False)
